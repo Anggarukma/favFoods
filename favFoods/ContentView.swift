39a36a65fa0a679b888095d692b594e1aa782ca5
Hirtanto Anggarukma <hirtantoangga@gmail.com>
2022-07-17 13:22:55 +0700

    Initial Commit

---
+++ favFoods/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  favFoods
+//
+//  Created by Hirtanto Angga on 17/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ favFoods/favFoodsApp.swift
@@ -0,0 +1,17 @@
+//
+//  favFoodsApp.swift
+//  favFoods
+//
+//  Created by Hirtanto Angga on 17/07/22.
+//
+
+import SwiftUI
+
+@main
+struct favFoodsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
